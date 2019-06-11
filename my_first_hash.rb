03110ea6472c75a2bb7a951e2850bd9867403fd4
Connor Cowan <ccowan003@comcast.net>
2019-06-11 22:33:32 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,6 +1,6 @@
 def my_hash
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
-  my_hash = {"name" => "Connor"}
+  my_hash = {"name" => "Connor", "age" => 29}
 
 end
 
